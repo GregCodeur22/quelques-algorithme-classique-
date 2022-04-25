d5ff057cf1d61c54bc5cd5e57cee6fd90088dbe0
Grégory <gregory.arside@gmail.com>
2022-04-25 18:41:30 +0200

    recherche dans une liste

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const prices = [1, 39, 25, 112, 111, 30, 211, 300, 5, 67];
+// const prices = [1, 39, 25, 112, 111, 30, 211, 300, 5, 67];
 
-let min = prices[0];
-for (const n of prices) {
-  if (n < min) {
-    min = n;
+// let min = prices[0];
+// for (const n of prices) {
+//   if (n < min) {
+//     min = n;
+//   }
+// }
+// console.log(min);
+
+// ------------------------------------------------------------------
+
+// Voici les dix derniers gagnants de la coupe départementale de Quidditch. Compléter le code suivant pour afficher les indices des valeurs de la liste égales à Broom broom. Combien de fois ont-ils gagné ?
+
+const teams = [
+  "Bois mort",
+  "Broom broom",
+  "Broom broom",
+  "Snek",
+  "Snek",
+  "Merlin FTW",
+  "Gandalf FTW",
+  "Merlin FTW",
+  "Broom broom",
+  "Bois mort",
+];
+const val = "Broom broom"; // La valeur à trouver
+
+for (let i = 0; i < teams.length; i++) {
+  if (val === teams[i]) {
+    console.log(i);
   }
 }
-console.log(min);
